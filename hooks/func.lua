6893aff3d6596d7cdd11d85eaa54ebecb8e5ce35
AllyTally <alexiatilde@gmail.com>
2020-09-22 17:40:01 -0300

    keep compat with love2d 9/10

--- hooks/func.lua
+++ hooks/func.lua
@@ -7,8 +7,7 @@ function register_cmd(name,func)
     table.insert(fakecommands,cmd)
 end
 
-info = love.filesystem.getInfo("fakecommands.lua")
-if info == nil then
+if not love.filesystem.exists("fakecommands.lua") then
     local success, message = love.filesystem.write("fakecommands.lua", "-- see fakecommands_defaults.lua in the plugin directory for examples.\n-- make sure to wrap your arguments in anythingbutnil0()!")
 end
 
