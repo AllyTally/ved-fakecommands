8dc0caad73e01f09495acc268b265fec7b8e9272
Reese Rivers <44736084+Fussmatte@users.noreply.github.com>
2022-11-04 19:36:56 -0400

    Add syntax highlighting support

--- sourceedits.lua
+++ sourceedits.lua
@@ -84,6 +84,216 @@ sourceedits =
 			ignore_error = false,
 			luapattern = false,
 			allowmultiple = false,
+		},
+		{
+			find = [[
+					local intsc = internalscript or cutscenebarsinternalscript
+					local is_sim = knowncommands[v_parsed:lower()]
+					local is_int = knowninternalcommands[v_parsed]
+]],
+			replace = [[
+					local intsc = internalscript or cutscenebarsinternalscript
+					local is_sim = knowncommands[v_parsed:lower()]
+					local is_int = knowninternalcommands[v_parsed]
+
+					local fc_k, fc_v
+					local is_fakecommand = false
+					for fc_k, fc_v in ipairs(fakecommands) do
+						if fc_v.name == utf8.sub(v_parsed,2,utf8.len(v_parsed)) then
+							is_fakecommand = true
+						end
+					end
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false
+		},
+		{
+			find = [[
+					elseif editing_command then
+						setColorArr(s.syntaxcolor_command)
+]],
+		    replace = [[
+					elseif utf8.sub(v_parsed,1,1)==":" and is_fakecommand then
+						setColorArr(s.syntaxcolor_fakecommand)
+					elseif editing_command then
+						setColorArr(s.syntaxcolor_command)
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		},
+		{
+			find = [[
+	do
+		local intsc = internalscript or cutscenebarsinternalscript
+		local is_sim = knowncommands[parts[1]:lower()]
+]],
+			replace = [[
+	do
+		local intsc = internalscript or cutscenebarsinternalscript
+		local is_sim = knowncommands[parts[1]:lower()]
+
+		local fc_k, fc_v
+		local is_fakecommand = false
+		for fc_k, fc_v in ipairs(fakecommands) do
+			if fc_v.name == utf8.sub(parts[1],2,utf8.len(parts[1])) then
+				is_fakecommand = true
+			end
+		end
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false
+		},
+		{
+			find = [[
+		if (not intsc and is_sim)
+		or (intsc and is_int) then
+			-- pass
+]],
+		    replace = [[
+		if (not intsc and is_sim)
+		or (intsc and is_int)
+		or (utf8.sub(parts[1],1,1)==":" and is_fakecommand) then
+			-- pass
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
 		}
-	}
+	},
+	["loadconfig"] =
+	{
+		{
+			find = [[
+	syntaxcolor_comment = {
+]],
+			replace = [[
+	syntaxcolor_fakecommand = {
+		default = {134, 255, 175},
+		["type"] = "numbersarray",
+	},
+	syntaxcolor_comment = {
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		}
+	},
+	["uis/syntaxoptions/draw"] =
+	{
+		{
+			find = [[
+	color_setting(L.SYNTAXCOLOR_COMMENT,    10, s.syntaxcolor_comment    )
+
+	checkbox(s.colored_textboxes, 8, 8+(24*12), "colored_textboxes", L.COLORED_TEXTBOXES,
+]],
+			replace = [[
+	color_setting(L.SYNTAXCOLOR_COMMENT,    10, s.syntaxcolor_comment    )
+	color_setting(L.SYNTAXCOLOR_FAKECOMMAND,11, s.syntaxcolor_fakecommand    )
+
+	checkbox(s.colored_textboxes, 8, 8+(24*13), "colored_textboxes", L.COLORED_TEXTBOXES,
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		}
+	},
+	["lang/en"] = --ENGLISH
+    {
+        {
+            find = [[L = {]],
+            replace = [[L = {
+SYNTAXCOLOR_FAKECOMMAND = "Fake command",
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		}
+    },
+    ["lang/eo"] = --ESPERANTO
+    {
+        {
+            find = [[L = {]],
+            replace = [[L = {
+SYNTAXCOLOR_FAKECOMMAND = "Falsa komando",
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		}
+    },
+    ["lang/fr"] = --FRENCH
+    {
+        {
+            find = [[L = {]],
+            replace = [[L = {
+SYNTAXCOLOR_FAKECOMMAND = "Fausse commande",
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		}
+    },
+    ["lang/es_AR"] = --SPANISH
+    {
+        {
+            find = [[L = {]],
+            replace = [[L = {
+SYNTAXCOLOR_FAKECOMMAND = "Comando falso",
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		}
+    },
+    ["lang/nl"] = --DUTCH
+    {
+        {
+            find = [[L = {]],
+            replace = [[L = {
+SYNTAXCOLOR_FAKECOMMAND = "Nep commando",
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		}
+    },
+    ["lang/de"] = --GERMAN
+    {
+        {
+            find = [[L = {]],
+            replace = [[L = {
+SYNTAXCOLOR_FAKECOMMAND = "Gefälschter Befehl",
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+        }
+    },
+    ["lang/ru"] = --RUSSIAN
+    {
+        {
+            find = [[L = {]],
+            replace = [[L = {
+SYNTAXCOLOR_FAKECOMMAND = "Поддельная команда",
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+        }
+    },
+    ["lang/id"] = --INDONESIAN
+    {
+        {
+            find = [[L = {]],
+            replace = [[L = {
+SYNTAXCOLOR_FAKECOMMAND = "Memerintah palsu",
+]],
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+        }
+    }
 }
